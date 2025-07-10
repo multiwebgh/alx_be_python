b2edb99648eedff08584e5bc684d524d23a380b7
Amankwah Foster <multiwebgh@gmail.com>
2025-07-10 20:46:49 +0000

    i added a simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+
+# Simple Interest Calculator
+# formula for simple interest is (I = P * R * T)
+
+principal = 1000 
+rate = 0.05
+time = 3
+interest = principal*rate*time
+print(f"The simple interest is: {interest}")
+
