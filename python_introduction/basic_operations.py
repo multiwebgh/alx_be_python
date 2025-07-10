3cef02c88cfa65921dc5e0a8c4ad4e4ea96d95ed
Amankwah Foster <multiwebgh@gmail.com>
2025-07-10 18:56:53 +0000

    my first python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+number1 = 10 
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum} ")
+print(f"Subtraction of {number1} and {number2} is {difference} ")
+print(f"product of  {number1} and {number2} is {product} ")
+
+
+
+
+
+# Addition of 10 and 5 is 15
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50
